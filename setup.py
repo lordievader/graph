06efb91ffa558dbe893c949bcb41a5f2711728d1
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2019-01-08 11:54:48 +0100

    Add setup.py and move things

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='graph',
+    version='1.0',
+    description='Graph library based on NetworkX',
+    author='Olivier van der Toorn',
+    author_email='oliviervdtoorn@gmail.com',
+    packages=['graph'],
+    install_requires=['matplotlib', 'networkx'],
+)
